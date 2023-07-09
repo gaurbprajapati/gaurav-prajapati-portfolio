f2be50f01996e415451e6dd906787c805d515989
Gaurav Prajapati <gauravprajapati094@gmail.com>
2023-07-09 15:30:47 +0530

    updating loader

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
-import React from 'react'
-import "./App.css"
-import About from './components/container/About/About'
-import Contact from './components/container/Contact/Contact'
-import Footer from './components/container/Footer/Footer'
-import Home from './components/container/Home/Home'
-// import Portfolio from './components/container/Portfolio/Portfolio'
-import Skills from './components/container/Skills/Skills'
-import Navbar from './components/Navbar/Navbar'
-
-import ClubCard from './components/ClubCard/Clubcard.jsx'
-
-// import random from './ramdom'
-
-// import Random from './ramdom'
+import React, { useState, useEffect } from 'react';
+import "./App.css";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import About from './components/container/About/About';
+import Contact from './components/container/Contact/Contact';
+import Footer from './components/container/Footer/Footer';
+import Home from './components/container/Home/Home';
+import Skills from './components/container/Skills/Skills';
+import Navbar from './components/Navbar/Navbar';
+import ClubCard from './components/ClubCard/Clubcard.jsx';
 
 const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Home />
-      <About />
-      <Skills />
-      <ClubCard />
-      {/* <Portfolio /> */}
-      <Contact />
-      <Footer />
-      {/* <Random /> */}
-    </div>
-  )
-}
+  const [loading, setLoading] = useState(true);
 
-export default App
+  useEffect(() => {
+    // Simulating a delay for demonstration purposes
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <div className="loader-container">
+          <Loader
+            type="ThreeDots"
+            color="#00BFFF"
+            height={80}
+            width={80}
+            timeout={10000}
+          />
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Home />
+          <About />
+          <Skills />
+          <ClubCard />
+          <Contact />
+          <Footer />
+        </>
+      )}
+    </>
+  );
+};
+
+export default App;
--- src/components/Maincard/Maincard.jsx
+++ src/components/Maincard/Maincard.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { motion } from "framer-motion";
 import "./Maincard.scss";
-// import { Link } from "react-router-dom";
-import {FiGithub, FiEye} from "react-icons/fi"
-
+import { Link } from "react-router-dom";
+import { FiGithub, FiEye } from "react-icons/fi";
+import { BrowserRouter as Router } from "react-router-dom";
 function Maincard(props) {
   return (
+    
     <motion.div
       initial={{ opacity: 0 }}
       whileInView={{ y: [40, 0], opacity: 1 }}
@@ -13,62 +14,54 @@ function Maincard(props) {
     >
       <div className="body_main">
         {props.data?.map((dataval, index) => (
-          <>
-            {/* <Link className="links" to={dataval.link}> */}
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="body_inner"
+            key={index} // Add unique key prop
+          >
+            <a target="_black" className="links" href={dataval.github}>
+              
+              <img src={dataval.img} alt={dataval.name} />
+              <h2 className="bold-text" style={{ marginTop: 20 }}>
+                {dataval.name}
+              </h2>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {dataval.discription.slice(0, 147)}
+              </p>
+
               <motion.div
-                whileInView={{ opacity: 1 }}
-                whileHover={{ scale: 1.1 }}
-                transition={{ duration: 0.5, type: "tween" }}
-                className="body_inner"
-                key={dataval.id}
-              >
-                <img src={dataval.img} alt={dataval.name} />
-                <h2 className="bold-text" style={{ marginTop: 20 }}>
-                  {dataval.name}
-                </h2>
-                <p className="p-text" style={{ marginTop: 10 }}>
-                  {dataval.discription.slice(0,147)}
-                </p>
-
-
-
-                <motion.div
-                initial={{opacity: 0}}
+                initial={{ opacity: 0 }}
                 whileHover={{ opacity: [0, 1] }}
-                transition={{duration: 0.3 , ease: "easeInOut"}}
-                className='hoverLayer'
+                transition={{ duration: 0.3, ease: "easeInOut" }}
+                className="hoverLayer"
               >
-
-
-                <motion.a href={dataval.github}
-                whileInView={{scale: [0,1]}}
-                 whileHover={{scale: [1, 1.1]}}
-                 transition={{duration: 0.3}}
-                 target="_black"
+                <motion.a
+                  href={dataval.github}
+                  whileInView={{ scale: [0, 1] }}
+                  whileHover={{ scale: [1, 1.1] }}
+                  transition={{ duration: 0.3 }}
+                  target="_blank"
                 >
                   <FiGithub />
                 </motion.a>
-                  
 
-                <motion.a href={dataval.live}
-                whileInView={{scale: [0,1]}}
-                 whileHover={{scale: [1, 1.1]}}
-                 transition={{duration: 0.3}}
+                <motion.a
+                  href={dataval.live}
+                  whileInView={{ scale: [0, 1] }}
+                  whileHover={{ scale: [1, 1.1] }}
+                  transition={{ duration: 0.3 }}
                 >
                   <FiEye />
                 </motion.a>
-
-                </motion.div>
-
-
               </motion.div>
-
-
-
-          </>
+            </a>
+          </motion.div>
         ))}
       </div>
     </motion.div>
+  
   );
 }
 
